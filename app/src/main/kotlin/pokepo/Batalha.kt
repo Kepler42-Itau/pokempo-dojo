f072c904410946e4a10281122b50f8ec13656a08
mde-figu <markusfwirz@gmail.com>
2021-10-14 15:34:19 -0300

    teste pipeline

--- app/src/main/kotlin/pokepo/Batalha.kt
+++ app/src/main/kotlin/pokepo/Batalha.kt
@@ -1,22 +1,22 @@
 package pokepo
 
-fun batalha(jogador1: String, jogador2: String): String {
+fun batalha(jogador1: String, jogador2: String)=
     if (jogador1 == "charmander"){
         if (jogador2 == "squirtle")
-            return "squirtle"
-        return "charmander"
+            "squirtle"
+        else "charmander"
     }
 
-    if (jogador1 == "bulbassauro"){
+    else if (jogador1 == "bulbassauro"){
         if (jogador2 == "charmander")
-            return "charmander"
-        return "bulbassauro"
+            "charmander"
+        else "bulbassauro"
     }
 
-    if (jogador1 == "squirtle"){
+    else if (jogador1 == "squirtle"){
         if (jogador2 == "bulbassauro")
-            return "bulbassauro"
-        return "squirtle"
+            "bulbassauro"
+        else "squirtle"
     }
-    return " "
-}
+    else
+        " "
